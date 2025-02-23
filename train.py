565003e2a678c02144c07ec4d994a97f640bc48a
SC0d3r <sasan.kelishani@gmail.com>
2025-02-23 13:38:21 +0330

    add comment for clip grad norm

--- train.py
+++ train.py
@@ -105,6 +105,7 @@ def td_lambda_update_trajectory(model, trajectory, final_reward, optimizer, lamb
 
     optimizer.zero_grad()
     loss.backward()
+    # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
     optimizer.step()
 
     return loss
